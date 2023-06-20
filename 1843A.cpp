377ddccdbe2a6400114ef29e06609a1747cdec7b
Rabbi_Hasan <108593765+Rabbi-hasan0@users.noreply.github.com>
2023-06-21 00:43:48 +0600

    A. Sasha and Array Coloring

---
+++ 1843A.cpp
@@ -0,0 +1,71 @@
+/**
+**     Author: Rabbi Hasan
+**     Bangladesh University of Business and Technology,
+**     Dept. of CSE.
+***/
+/* header file */
+#include<bits/stdc++.h>
+#include<stack>
+#include <queue>
+#include <algorithm>
+#include<iostream>
+#include<string>
+/* for faster cin cout */
+#define fast() ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+/* sort function */
+#define strsort() sort(s.begin(),s.end())
+#define arrsort() int n1=sizeof(a)/sizeof(a[0]);sort(a,a+n1)
+/* legth function */
+#define strlen s.size()
+#define arrlen int n1=sizeof(a)/sizeof(a[0])
+#define intlen int n2=trunc(log10(n))+1
+/* Conver function */
+#define csti int csti=stoi(s)
+/* Others Sortcut */
+#define PI 3.1415926535897932384626433832795
+#define nn "\n"
+#define yes "YES"
+#define no "NO"
+#define case "Case "
+#define elif else if
+#define ll long long
+#define lli long long int
+#define pb push_back
+/* testcase */
+#define test() int t;cin>>t;while(t--)
+#define testt() int t;cin>>t;for(int i=1; i<=t; i++)
+#define f(i,n) for(lli i = 0; i < n; i++)
+using namespace std;
+const ll mod = 1e9 + 7;
+//const ll N = 2e5 + 10;
+/*  let's started  */
+void solved()
+{
+    test()
+    {
+      int n;cin>>n;
+      int a[n+1];
+      f(i,n)cin>>a[i];
+      sort(a,a+n);
+      int i=0;
+      int j=n-1;
+      int ans=0;
+      while(i<j)
+      {
+        //cout<<a[j]<<' '<<a[i]<<nn;
+        int sum=a[j]-a[i];
+        i++;
+        j--;
+        ans+=sum;
+      }
+      cout<<ans<<nn;
+    }
+
+}
+
+int main()
+{
+    fast();
+    solved();
+    return 0;
+}
