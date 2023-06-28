7d8b166a9f0894f7752895ed4510b68181499103
Rabbi_Hasan <108593765+Rabbi-hasan0@users.noreply.github.com>
2023-06-28 15:25:58 +0600

    D. Diverse Garland

---
+++ 1108D.cpp
@@ -0,0 +1,79 @@
+/**
+**     Author: Rabbi Hasan
+**     Bangladesh University of Business and Technology,
+**     Dept. of CSE.
+***/
+/* header file */
+#include<bits/stdc++.h>
+#include<stack>
+#include <queue>
+#include <algorithm>
+#include<iostream>
+#include<string>
+/* for faster cin cout */
+#define fast() ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+/* sort function */
+#define strsort() sort(s.begin(),s.end())
+#define arrsort() int n1=sizeof(a)/sizeof(a[0]);sort(a,a+n1)
+/* legth function */
+#define strlen s.size()
+#define arrlen int n1=sizeof(a)/sizeof(a[0])
+#define intlen int n2=trunc(log10(n))+1
+/* Conver function */
+#define csti int csti=stoi(s)
+/* Others Sortcut */
+#define PI 3.1415926535897932384626433832795
+#define nn "\n"
+#define yes "YES"
+#define no "NO"
+#define case "Case "
+#define elif else if
+#define ll long long
+#define lli long long int
+#define pb push_back
+/* testcase */
+#define test() int t;cin>>t;while(t--)
+#define testt() int t;cin>>t;for(int i=1; i<=t; i++)
+#define f(i,n) for(int i = 0; i < n; i++)
+using namespace std;
+const ll mod = 1e9 + 7;
+//const ll N = 2e5 + 10;
+/*  let's started  */
+void solved()
+{
+    ll n,cnt=0;
+    cin>>n;
+    string s;
+    cin>>s;
+    f(i,s.size()-1)
+    {
+        if(s[i]==s[i+1])
+        {
+            cnt++;
+            if(i+2==s.size())
+            {
+                if(s[i]!='B')
+                    s[i+1]='B';
+                elif(s[i]!='R')
+                s[i+1]='R';
+                else s[i+1]='G';
+            }
+            elif(s[i]!='B'&&s[i+2]!='B')
+            s[i+1]='B';
+            elif(s[i]!='R'&&s[i+2]!='R')
+            s[i+1]='R';
+            else s[i+1]='G';
+        }
+
+    }
+    cout<<cnt<<nn;
+    cout<<s<<nn;
+}
+
+int main()
+{
+    fast();
+    //test()
+    solved();
+    return 0;
+}
