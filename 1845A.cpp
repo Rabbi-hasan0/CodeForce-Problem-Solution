ba2b1bad4e469acde3994728a016aab4e274f78e
Rabbi_Hasan <108593765+Rabbi-hasan0@users.noreply.github.com>
2023-06-30 00:34:36 +0600

    A. Forbidden Integer

---
+++ 1845A.cpp
@@ -0,0 +1,79 @@
+/**
+**     Author: Rabbi Hasan
+**     Bangladesh University of Business and Technology,
+**     Dept. of CSE.
+***/
+/* header file */
+#include<bits/stdc++.h>
+#include<stack>
+#include <queue>
+#include <algorithm>
+#include<iostream>
+#include<string>
+/* for faster cin cout */
+#define fast() ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+/* sort function */
+#define strsort() sort(s.begin(),s.end())
+#define arrsort() int n1=sizeof(a)/sizeof(a[0]);sort(a,a+n1)
+/* legth function */
+#define strlen s.size()
+#define arrlen int n1=sizeof(a)/sizeof(a[0])
+#define intlen int n2=trunc(log10(n))+1
+/* Conver function */
+#define csti int csti=stoi(s)
+/* Others Sortcut */
+#define PI 3.1415926535897932384626433832795
+#define nn "\n"
+#define yes "YES"
+#define no "NO"
+#define case "Case "
+#define elif else if
+#define ll long long
+#define lli long long int
+#define pb push_back
+/* testcase */
+#define test() int t;cin>>t;while(t--)
+#define testt() int t;cin>>t;for(int i=1; i<=t; i++)
+#define f(i,n) for(int i = 0; i < n; i++)
+using namespace std;
+const ll mod = 1e9 + 7;
+//const ll N = 2e5 + 10;
+/*  let's started  */
+void solved()
+{
+    int n,k,x;
+    cin>>n>>k>>x;
+
+    if(x!=1&&k>=1)
+    {
+        cout<<yes<<nn<<n<<nn;
+        while(n--)cout<<1<<' ';
+        cout<<nn;
+    }
+    elif(n%2==0&&x==1&&k>1)
+    {
+        n/=2;
+        cout<<yes<<nn<<n<<nn;
+        while(n--)cout<<2<<' ';
+        cout<<nn;
+    }
+    elif(n&1 &&x==1&&k>=3)
+    {
+        int c=0;
+        n-=3;
+        n/=2;
+        cout<<yes<<nn<<n+1<<nn;
+        cout<<3<<' ';
+        while(n--)cout<<2<<' ';
+        cout<<nn;
+    }
+    else
+        cout<<no<<nn;
+}
+int main()
+{
+    fast();
+    test()
+    solved();
+    return 0;
+}
